be3b4d2bd3b30b6f53f05e80d87b05ea44bfbc9a
Fran Invernot <francisco.invernot_sanchez@roche.com>
2023-10-09 13:59:27 +0200

    fran-model-v4

--- fran_model.py
+++ fran_model.py
@@ -22,9 +22,9 @@ def reward_function(params):
     # constants
     MAX_REWARD = 1e2
     MIN_REWARD = 1e-3
-    DIRECTION_THRESHOLD = 10.0
     ABS_STEERING_THRESHOLD = 30
     STEPS_THRESHOLD = 300
+    SPEED_THRESHOLD = 3.0
 
     def all_wheels_on_track_reward(current_reward):
         if not all_wheels_on_track:
@@ -42,26 +42,26 @@ def reward_function(params):
 
     def distance_from_center_reward(current_reward, distance):
         # Calculate markers that are at varying distances away from the center line
-        marker_1 = 0.2 * track_width
-        marker_2 = 0.4 * track_width
-        marker_3 = 0.7 * track_width
-        marker_4 = 0.9 * track_width
+        marker_1 = 0.1 * track_width
+        marker_2 = 0.2 * track_width
+        marker_3 = 0.3 * track_width
+        marker_4 = 0.5 * track_width
         # Give higher reward if the car is closer to center line and vice versa
         if distance <= marker_1:
             current_reward *= 1.5
         elif distance <= marker_2:
-            current_reward *= 1.1
+            current_reward *= 1.2
         elif distance <= marker_3:
-            current_reward *= 0.7
+            current_reward *= 0.9
         elif distance <= marker_4:
-            current_reward *= 0.3
+            current_reward *= 0.7
         else:
             current_reward = MIN_REWARD  # likely crashed/ close to off track
         return current_reward
 
     def follow_the_center_line(current_reward):
         # Positive reward if the car is in a straight line going fast
-        if abs_steering_angle < 0.1 and speed > 3:
+        if abs_steering_angle < 0.1 and speed > (SPEED_THRESHOLD * 0.8):
             current_reward *= 1.2
         return current_reward
 
@@ -75,9 +75,29 @@ def reward_function(params):
         suggested_direction = math.degrees(suggested_direction)
         # Calculate difference between track direction and car heading angle
         direction_diff = abs(suggested_direction - heading)
-        # Penalize if the difference is too large
-        if direction_diff > DIRECTION_THRESHOLD:
-            current_reward *= 0.5
+
+        marker_direction_1 = 1.0
+        marker_direction_2 = 2.0
+        marker_direction_3 = 3.0
+        marker_direction_4 = 4.0
+        marker_direction_5 = 5.0
+        marker_direction_10 = 10.0
+
+        if direction_diff <= marker_direction_1:
+            current_reward *= 1.6
+        elif direction_diff <= marker_direction_2:
+            current_reward *= 1.4
+        elif direction_diff <= marker_direction_3:
+            current_reward *= 1.2
+        elif direction_diff <= marker_direction_4:
+            current_reward *= 1.0
+        elif direction_diff <= marker_direction_5:
+            current_reward *= 0.8
+        elif direction_diff <= marker_direction_10:
+            current_reward *= 0.6
+        else:
+            current_reward *= 0.4
+
         return current_reward
 
     def stay_inside_the_two_borders(current_reward):
@@ -109,7 +129,7 @@ def reward_function(params):
     reward = follow_the_center_line(reward)
     reward = direction(reward)
     reward = steps_reward(reward)
-    reward = stay_inside_the_two_borders(reward)
+    # reward = stay_inside_the_two_borders(reward)
     reward = prevent_zig_zag(reward)
     reward = throttle(reward)
 
